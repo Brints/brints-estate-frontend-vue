719c22c6dc9f5e4244025ebb4dfbd15ec5d70c3f
aniebietafia <aniebietafia87@gmail.com>
2024-03-16 10:04:39 +0100

    feat: initialized landing page

---
+++ brints-estate-frontend/src/views/users/LandingPage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed } from 'vue'
+</script>
+
+<template>
+  <div :class="$style.wrapper">
+    <h1>Brints Estate Frontend - Landing Page</h1>
+    <p>Where you can find your dream home</p>
+  </div>
+</template>
+
+<style module>
+.wrapper {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  place-items: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
