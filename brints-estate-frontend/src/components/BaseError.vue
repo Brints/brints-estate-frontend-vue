4f68a9aa3406eca9d2ee065ff52cc968c8b6b6e8
aniebietafia <aniebietafia87@gmail.com>
2024-03-19 22:16:28 +0100

    feat: base error component

---
+++ brints-estate-frontend/src/components/BaseError.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div v-if="message">{{ message }}</div>
+</template>
