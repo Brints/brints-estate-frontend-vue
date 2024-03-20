0b95ebd3021c1b478cfedca43da44da6cf446dc2
aniebietafia <aniebietafia87@gmail.com>
2024-03-20 13:54:57 +0100

    feat: added landing page route

--- brints-estate-frontend/src/router/index.ts
+++ brints-estate-frontend/src/router/index.ts
@@ -1,29 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LandingPage from "@/views/users/LandingPage.vue";
+import LandingPage from "../views/users/LandingPage.vue";
+// import LandingPage from "@/views/users/LandingPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
       name: "landing-page",
       component: LandingPage,
     },
-    {
-      path: "/register",
-      name: "signup",
-      component: () => import("@/views/users/SignupPage.vue"),
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("@/views/users/LoginPage.vue"),
-    },
-    {
-      path: "/user/:token",
-      name: "verify-email",
-      component: () => import("@/views/users/VerifyEmailPage.vue"),
-    },
+    // {
+    //   path: "/register",
+    //   name: "signup",
+    //   component: () => import("@/views/users/SignupPage.vue"),
+    // },
   ],
 });
 
