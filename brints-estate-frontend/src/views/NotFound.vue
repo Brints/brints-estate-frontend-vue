17a728e196964fc05c4eddb27658ce94675cbdc5
aniebietafia <aniebietafia87@gmail.com>
2024-03-21 09:10:11 +0100

    feat: added not found page

---
+++ brints-estate-frontend/src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h4>404 Page - Not Found</h4>
+    <p>404 Page</p>
+    <div>The path: {{ $route.params.path }} does not exist.</div>
+  </div>
+</template>
+
+<style module></style>
