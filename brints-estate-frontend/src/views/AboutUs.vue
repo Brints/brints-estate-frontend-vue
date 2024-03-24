9810593a1537b5db71baa183cc64bd88f10317d2
aniebietafia <aniebietafia87@gmail.com>
2024-03-24 13:06:38 +0100

    feat: queried the about us endpoint

---
+++ brints-estate-frontend/src/views/AboutUs.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const aboutUs = ref(null);
+const loading = ref(false);
+
+onMounted(() => {
+  fetchAboutUs();
+});
+
+async function fetchAboutUs() {
+  try {
+    loading.value = true;
+    const response = await axios.get("http://localhost:3001/admin/about-us");
+    aboutUs.value = response.data.payload;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>About Us</h1>
+    <section v-if="loading">Loading...</section>
+    <section v-else-if="!loading && !aboutUs">No Content yet.</section>
+    <section v-else>
+      <div v-for="item in aboutUs" :key="item.aboutId">
+        <h1>{{ item.title }}</h1>
+        <img :src="item.image[0].url" :alt="item.image[0].filename" />
+        <p>{{ item.content }}</p>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
