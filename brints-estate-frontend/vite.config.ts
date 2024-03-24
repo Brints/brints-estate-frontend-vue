a99f5e3a8a31fdbb8b2c1bd98993b77b5aa372e0
aniebietafia <aniebietafia87@gmail.com>
2024-03-24 13:07:38 +0100

    settings: updated project configuration settings

--- brints-estate-frontend/vite.config.ts
+++ brints-estate-frontend/vite.config.ts
@@ -12,11 +12,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-// });
