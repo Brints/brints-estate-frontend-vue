561c8bd9c4c2cc5efd66cf88114d8759b6d5338b
aniebietafia <aniebietafia87@gmail.com>
2024-03-26 09:43:16 +0100

    feat: initialized signup page

--- brints-estate-frontend/src/views/users/SignupPage.vue
+++ brints-estate-frontend/src/views/users/SignupPage.vue
@@ -1,8 +1,55 @@
-<script setup></script>
+<script setup>
+// import { ref } from "vue";
+import BaseForm from "@/components/BaseForm.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import LabelInput from "@/components/LabelInput.vue";
+import LabelSelect from "@/components/LabelSelect.vue";
+</script>
 
 <template>
-  <router-link to="/">Landing Page</router-link>
-  <h1>Signup Page</h1>
+  <main :class="$style.wrapper">
+    <BaseForm>
+      <template #legend>
+        <h1>Signup</h1>
+      </template>
+      <template #content>
+        <LabelInput
+          label="Full Name"
+          id="full-name"
+          name="full-name"
+          type="text"
+          placeholder="Enter your full name"
+        />
+        <LabelInput
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+        />
+        <LabelInput
+          label="Password"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <LabelSelect label="Role" id="role" name="role">
+          <option value="">Choose a Role</option>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </LabelSelect>
+        <BaseButton type="submit">Signup</BaseButton>
+      </template>
+    </BaseForm>
+  </main>
 </template>
 
-<style module></style>
+<style module>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
