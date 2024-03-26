889c006889f68f0cc19a160ab44b46c23fe568ff
aniebietafia <aniebietafia87@gmail.com>
2024-03-26 09:42:34 +0100

    feat: added form component

---
+++ brints-estate-frontend/src/components/BaseForm.vue
@@ -0,0 +1,37 @@
+<script></script>
+
+<template>
+  <form :class="$style.main_form">
+    <fieldset>
+      <legend>
+        <slot name="legend"></slot>
+      </legend>
+      <slot name="content"></slot>
+    </fieldset>
+  </form>
+</template>
+
+<style module>
+.main_form {
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+}
+
+fieldset {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px 0;
+}
+
+legend {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+  padding: 0 10px;
+}
+</style>
