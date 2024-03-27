8b9c54039c7ecb78da946ae904b35802ae4d165a
aniebietafia <aniebietafia87@gmail.com>
2024-03-27 15:33:06 +0100

    feat: added reset button component

---
+++ brints-estate-frontend/src/components/ResetButton.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <button
+    type="reset"
+    class="reset-button bg-red-700 rounded-md py-2.5 px-5 border-0 capitalize text-white font-semibold hover:bg-red-800 transition-all duration-300 ease-in-out"
+  >
+    <slot></slot>
+  </button>
+</template>
