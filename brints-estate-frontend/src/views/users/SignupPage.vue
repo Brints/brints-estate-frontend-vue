73f108d09b6a2bda9eb25d826bd65af8919604dd
aniebietafia <aniebietafia87@gmail.com>
2024-03-27 14:14:45 +0100

    feat: imported and used the image input component

--- brints-estate-frontend/src/views/users/SignupPage.vue
+++ brints-estate-frontend/src/views/users/SignupPage.vue
@@ -4,41 +4,64 @@ import BaseForm from "@/components/BaseForm.vue";
 import BaseButton from "@/components/BaseButton.vue";
 import LabelInput from "@/components/LabelInput.vue";
 import LabelSelect from "@/components/LabelSelect.vue";
+import ImageInput from "../../components/ImageInput.vue";
 </script>
 
 <template>
   <main :class="$style.wrapper">
     <BaseForm>
       <template #legend>
-        <h1>Signup</h1>
+        <h1>Sign Up</h1>
       </template>
       <template #content>
-        <LabelInput
-          label="Full Name"
-          id="full-name"
-          name="full-name"
-          type="text"
-          placeholder="Enter your full name"
-        />
-        <LabelInput
-          label="Email"
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Enter your email"
-        />
-        <LabelInput
-          label="Password"
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-        <LabelSelect label="Role" id="role" name="role">
-          <option value="">Choose a Role</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </LabelSelect>
+        <ImageInput label="Upload your Avatar" id="avatar" name="avatar" />
+        <section :class="$style.form_content">
+          <section class="left-column">
+            <LabelInput
+              label="Full Name"
+              id="fullname"
+              name="fullname"
+              type="text"
+              placeholder="Enter your full name e.g. John Doe"
+            />
+            <LabelInput
+              label="Email"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email e.g. johndoe@example.com"
+            />
+            <LabelInput
+              label="Password"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </section>
+          <section class="right-column">
+            <LabelInput
+              label="Phone Number"
+              id="phone"
+              name="phone"
+              type="tel"
+              placeholder="Enter your phone number"
+            />
+            <LabelSelect label="Gender" id="gender" name="gender">
+              <option value="">Choose your Gender</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+            </LabelSelect>
+            <LabelInput
+              label="Confirm Password"
+              id="confirm-password"
+              name="confirmPassword"
+              type="password"
+              placeholder="Repeat your Password"
+            />
+          </section>
+        </section>
+
         <BaseButton type="submit">Signup</BaseButton>
       </template>
     </BaseForm>
@@ -51,5 +74,11 @@ import LabelSelect from "@/components/LabelSelect.vue";
   justify-content: center;
   align-items: center;
   height: 100vh;
+}
+
+.form_content {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
 }
 </style>
