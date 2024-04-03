d4275f3e5a04c96b601cc9773db6eb350532cd53
aniebietafia <aniebietafia87@gmail.com>
2024-04-03 15:36:32 +0100

    fix: fetched and used image as background image

--- brints-estate-frontend/src/views/AboutUs.vue
+++ brints-estate-frontend/src/views/AboutUs.vue
@@ -1,9 +1,11 @@
-<script setup>
+<!-- <script setup>
 import { ref, onMounted } from "vue";
 import axios from "axios";
+import HeaderBar from "@/components/HeaderBar.vue";
 
-const aboutUs = ref(null);
-const loading = ref(false);
+let aboutUs = ref(null);
+let imageUrl = ref("");
+let loading = ref(false);
 
 onMounted(() => {
   fetchAboutUs();
@@ -16,57 +18,134 @@ async function fetchAboutUs() {
     loading.value = true;
     const response = await axios.get(`${url}/admin/about-us`);
     aboutUs.value = response.data.payload;
+    for (const item of aboutUs.value) {
+      imageUrl.value = item.image[0].url;
+    }
+    console.log(imageUrl.value);
   } catch (error) {
     console.error(error);
   } finally {
     loading.value = false;
   }
 }
+</script> -->
+
+<script>
+import axios from "axios";
+import HeaderBar from "@/components/HeaderBar.vue";
+
+export default {
+  components: {
+    HeaderBar,
+  },
+  data() {
+    return {
+      imageUrl: "",
+      loading: false,
+      aboutUs: null,
+    };
+  },
+  async mounted() {
+    this.fetchAboutUs();
+  },
+  methods: {
+    async fetchAboutUs() {
+      const url = import.meta.env.VITE_BACKEND_URL;
+      try {
+        this.loading = true;
+        const response = await axios.get(`${url}/admin/about-us`);
+        this.aboutUs = response.data.payload;
+        for (const item of this.aboutUs) {
+          this.imageUrl = item.image[0].url;
+        }
+        console.log(this.imageUrl);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
 </script>
+
 <template>
-  <main className="flex flex-col min-h-screen">
-    <nav className="h-20">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about-us">About Us</router-link>
-      <router-link to="/contact-us">Contact Us</router-link>
-    </nav>
-    <section v-if="loading" className="place-items-center">Loading...</section>
+  <div :class="$style.container">
+    <div class="nav-links">
+      <HeaderBar></HeaderBar>
+    </div>
 
-    <section v-else-if="aboutUs">
-      <div v-for="item in aboutUs" :key="item._id">
-        <h1>{{ item.title }}</h1>
-        <div
-          class="image relative bg-cover bg-center bg-no-repeat"
-          :style="{ backgroundImage: `url('${item.image[0].url}')` }"
-        >
-          <h1
-            className="bg-zinc-300 rounded-lg text-xl absolute top-1/2 left-5 w-80 p-6 text-emerald-700 "
+    <main className="flex flex-col h-full">
+      <section v-if="loading" :class="$style.loading">
+        <p>Loading...</p>
+      </section>
+
+      <section v-else-if="aboutUs" class="about-contents">
+        <div v-for="item in aboutUs" :key="item._id">
+          <div
+            :class="$style.imageContainer"
+            className="bg-center bg-cover bg-no-repeat"
+            :style="{
+              height: '100vh',
+              backgroundImage: `linear-gradient(rgb(83, 95, 110, 0.7), rgba(0, 0, 0, 0.7)), url('${imageUrl}')`,
+            }"
           >
-            Looking for apartment? Do you have a property for sale or lease?
-            Brints Estate got you covered.
-          </h1>
-        </div>
-        <p>{{ item.content }}</p>
-      </div>
-    </section>
+            <h1
+              className="bg-zinc-300 rounded-lg text-xl relative top-1/2 left-14 w-80 p-6 text-emerald-700 -translate-y-2/4 "
+            >
+              Looking for apartment? Do you have a property for sale or lease?
+              Brints Estate got you covered.
+            </h1>
+          </div>
 
-    <section class="no-content" v-else>
-      <p>No Content yet.</p>
-    </section>
-  </main>
+          <div className="mt-8">
+            <h1 className="lowercase text-5xl text-center border-b-2">
+              {{ item.title }}
+            </h1>
+            <p>{{ item.content }}</p>
+          </div>
+        </div>
+      </section>
+
+      <section v-else :class="$style.no_content">
+        <p>No Content yet.</p>
+      </section>
+    </main>
+  </div>
 </template>
 
-<style scoped>
-.no-content {
+<style module>
+.loading {
+  height: calc(100vh - 5rem);
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.imageContainer {
+  position: absolute;
+  width: 100%;
+  height: 100px;
+}
+
+.no_content {
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 1.5rem;
   height: calc(100vh - 5rem);
 }
 
-.image {
+/* .about-contents {
   width: 100%;
   height: calc(100vh - 5rem);
 }
+
+.image {
+  display: block;
+  width: 100%;
+  height: 100vh;
+} */
 </style>
