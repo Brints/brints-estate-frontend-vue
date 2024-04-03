576be3f5147c5aa9a522da9dc5d883ea059dbef6
aniebietafia <aniebietafia87@gmail.com>
2024-04-03 15:37:53 +0100

    feat: added header bar component

---
+++ brints-estate-frontend/src/components/HeaderBar.vue
@@ -0,0 +1,32 @@
+<script>
+import BaseLogo from "@/components/BaseLogo.vue";
+import HeaderBarLinks from "@/components/HeaderBarLinks.vue";
+export default {
+  components: {
+    BaseLogo,
+    HeaderBarLinks,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <header :class="$style.wrapper">
+    <BaseLogo></BaseLogo>
+    <HeaderBarLinks></HeaderBarLinks>
+  </header>
+</template>
+
+<style module>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 5rem;
+  padding: 0 2rem;
+}
+</style>
