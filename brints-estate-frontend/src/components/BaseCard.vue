d2ed39bb5fdc132198b607b52898612c149ed1d2
aniebietafia <aniebietafia87@gmail.com>
2024-04-06 11:06:39 +0100

    feat: initializing base card with shadow

---
+++ brints-estate-frontend/src/components/BaseCard.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div :class="$style.card">
+    <slot></slot>
+  </div>
+</template>
+
+<style module>
+.card {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+}
+
+.card + .card {
+  margin-top: 16px;
+}
+
+.card:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
