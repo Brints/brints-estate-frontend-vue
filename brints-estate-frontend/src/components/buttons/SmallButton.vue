a41b6093bb1e10949ae2d5c8da5db1b1f38cd743
aniebietafia <aniebietafia87@gmail.com>
2024-04-08 09:26:20 +0100

    feat: added small button component

---
+++ brints-estate-frontend/src/components/buttons/SmallButton.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+  <button type="submit" :class="$style.small_button">
+    <slot></slot>
+  </button>
+</template>
+
+<style module>
+.small_button {
+  background-color: #363333;
+  border: 1px solid #130f0f;
+  padding: 3px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+.small_button:hover {
+  filter: brightness(160%);
+  transition: all 1s ease-in-out;
+}
+</style>
