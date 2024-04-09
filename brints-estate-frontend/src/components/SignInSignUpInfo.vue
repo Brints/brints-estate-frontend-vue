5fdd9668b8da272f0d14d1847bf3cc25b354a84e
aniebietafia <aniebietafia87@gmail.com>
2024-04-09 10:28:50 +0100

    feat: sign up info component

--- brints-estate-frontend/src/components/SignInSignUpInfo.vue
+++ brints-estate-frontend/src/components/SignInSignUpInfo.vue
@@ -2,18 +2,6 @@
 
 <template>
   <section>
-    <!-- <p>
-      Do you have an account?
-      <span
-        className="text-lime-500 hover:text-lime-900 transition-all duration-300"
-        >Login</span
-      >
-      | Want to join the fun?
-      <span
-        className="text-red-500 hover:text-red-600 transition-all duration-300"
-        >Signup</span
-      >
-    </p> -->
     <slot></slot>
   </section>
 </template>
