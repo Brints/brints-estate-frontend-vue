1a287243a7a786fd56444bfc46913b594e3c1995
aniebietafia <aniebietafia87@gmail.com>
2024-04-16 11:59:22 +0100

    feat: added fuctionality to toggle password and update the fontawesome icon accordingly

--- brints-estate-frontend/src/components/form/BaseInput.vue
+++ brints-estate-frontend/src/components/form/BaseInput.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const props = defineProps({
   id: {
@@ -27,9 +29,26 @@ const props = defineProps({
     type: String,
     default: "",
   },
+  icon: {
+    type: String,
+    default: "",
+  },
+  special: {
+    type: String,
+    default: "",
+  },
+  error: {
+    type: String,
+    default: "",
+  },
 });
 
 const inputValue = ref(props.value);
+const isPasswordVisible = ref(false);
+
+const togglePassword = () => {
+  isPasswordVisible.value = !isPasswordVisible.value;
+};
 
 const watchValue = () => {
   inputValue.value = props.value;
@@ -45,19 +64,23 @@ watchInputValue();
 
 <template>
   <div :class="$style.form_group">
-    <label :for="id"
-      ><slot name="label">{{ label }}</slot></label
-    >
-    <slot name="input">
-      <input
-        :class="$style.form_control"
-        :type="type"
-        :id="id"
-        :name="name"
-        :placeholder="placeholder"
-        v-model.trim="inputValue"
-      />
-    </slot>
+    <div :class="$style.label_group">
+      <span><font-awesome-icon :icon="icon" /></span>
+      <label :for="id">{{ label }}<span class="text-red-500">*</span></label>
+    </div>
+
+    <input
+      :type="type === 'password' && isPasswordVisible ? 'text' : type"
+      :id="id"
+      :name="name"
+      :placeholder="placeholder"
+      v-model.trim="inputValue"
+    />
+
+    <span v-if="special" :class="$style.special" @click="togglePassword">
+      <font-awesome-icon :icon="isPasswordVisible ? 'eye' : 'eye-slash'" />
+    </span>
+    <slot name="error" :error="error"></slot>
   </div>
 </template>
 
@@ -71,7 +94,12 @@ watchInputValue();
   margin-bottom: 0.5rem;
 }
 
-.form_control {
+.label_group {
+  display: flex;
+  gap: 0.5rem;
+}
+
+input {
   width: 100%;
   height: 3rem;
   background-color: transparent;
@@ -80,49 +108,16 @@ watchInputValue();
   border: 1px solid #ccc;
   border-radius: 5px;
 }
+
+input:focus {
+  outline: none;
+  border-color: #2563eb;
+}
+
+.special {
+  position: relative;
+  cursor: pointer;
+  top: -2.27rem;
+  right: -21.17rem;
+}
 </style>
-
-<!-- <script>
-  export default {
-  props: {
-    id: {
-      type: String,
-      required: true,
-    },
-    label: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      default: "text",
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    placeholder: {
-      type: String,
-      default: "",
-    },
-    value: {
-      type: String,
-      default: "",
-    },
-  },
-
-  data() {
-    return {
-      inputValue: this.value,
-    };
-  },
-  watch: {
-    value(newValue) {
-      this.inputValue = newValue;
-    },
-    inputValue(newValue) {
-      this.$emit("input", newValue);
-    },
-  },
-};
-</script> -->
