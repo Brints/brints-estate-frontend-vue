03e39680ccb8ddf2ec24f94b3b5bc6435d27e857
aniebietafia <aniebietafia87@gmail.com>
2024-04-24 10:20:16 +0100

    main script

--- brints-estate-frontend/src/main.ts
+++ brints-estate-frontend/src/main.ts
@@ -3,16 +3,23 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+// import Vuelidate from "vuelidate";
+
 import App from "./App.vue";
 import router from "./router";
 
-// import HeaderBar from "../src/components/layout/HeaderBar.vue";
+library.add(fas);
 
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
 
-// app.component("header-bar", HeaderBar);
+// app.use(Vuelidate);
 
 app.mount("#app");
