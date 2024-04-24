012b8b66eddcbd2e191ac434923903bcb8117afe
aniebietafia <aniebietafia87@gmail.com>
2024-04-24 10:16:50 +0100

    feat: login page component

--- brints-estate-frontend/src/views/users/LoginPage.vue
+++ brints-estate-frontend/src/views/users/LoginPage.vue
@@ -1,14 +1,44 @@
 <script setup>
 import HeaderBar from "@/components/layout/HeaderBar.vue";
+import CountriesData from "@/lookup/country-codes.json";
+
+const countries = CountriesData.countries;
+const url = "https://emojicdn.elk.sh/";
 </script>
 
 <template>
   <div class="nav-links">
     <HeaderBar></HeaderBar>
   </div>
-  <div class="container">
+  <div :class="$style.container">
     <h1>Login Page</h1>
+    <ul>
+      <li v-for="country in countries" :key="country.code">
+        {{ country.name }} <span class="mr-5"><img :src="`${url}${country.flag}`" /></span> {{ country.zipcode }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<style module></style>
+<style module>
+ul {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-gap: 1rem;
+  list-style: none;
+  padding: 1rem;
+}
+
+ul li {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+}
+
+/* ul li img {
+  width: 30px;
+  height: 30px;
+} */
+</style>
