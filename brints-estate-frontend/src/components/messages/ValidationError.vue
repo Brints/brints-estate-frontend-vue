5c77d2e027bec6275f87be7ea58a8b97c5370ade
aniebietafia <aniebietafia87@gmail.com>
2024-04-29 14:32:59 +0100

    feat: adding validation error component

---
+++ brints-estate-frontend/src/components/messages/ValidationError.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  model: { required: true },
+});
+</script>
+
+<template>
+  <small class="text-red-500" v-if="model.$invalid">
+    <ul>
+      <li v-for="error in model.$errors" :key="error">{{ error.$message }}</li>
+    </ul>
+  </small>
+</template>
+
+<style scoped></style>
