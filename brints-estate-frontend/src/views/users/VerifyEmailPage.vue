0b9fb7a1e20fefcaeb14b4c669c4bde9333e748a
aniebietafia <aniebietafia87@gmail.com>
2024-05-02 15:49:16 +0100

    feat: added verify email page

--- brints-estate-frontend/src/views/users/VerifyEmailPage.vue
+++ brints-estate-frontend/src/views/users/VerifyEmailPage.vue
@@ -1,7 +1,34 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import SuccessMessage from "@/components/messages/SuccessMessage.vue";
+import BaseButton from "@/components/buttons/BaseButton.vue";
+
+let loading = ref(false);
+</script>
 
 <template>
-  <h1>Verify Email</h1>
+  <div :class="$style.container">
+    <div v-if="loading">
+      <p>Loading...</p>
+      <p>Please wait...</p>
+    </div>
+
+    <div v-else>
+      <SuccessMessage>
+        <p>Your email has been verified successfully.</p>
+      </SuccessMessage>
+      <!-- Login Button -->
+      <BaseButton type="button" class="btn btn-primary" @click="login"> Login </BaseButton>
+    </div>
+  </div>
 </template>
 
-<style module></style>
+<style module>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
