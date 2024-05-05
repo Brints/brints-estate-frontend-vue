8297e02910f5e238388bec78bd462969a56bbf23
aniebietafia <aniebietafia87@gmail.com>
2024-05-05 15:17:37 +0100

    feat: css styles for generate password button

--- brints-estate-frontend/src/components/buttons/SmallButton.vue
+++ brints-estate-frontend/src/components/buttons/SmallButton.vue
@@ -8,16 +8,19 @@ defineProps({
     type: String,
     required: true,
   },
+  button: {
+    type: String,
+  },
 });
 </script>
 
 <template>
-  <button :type="type" :label="label" :class="$style.small_button">
+  <button :type="type" :label="label" :class="[button, 'small_button']">
     <slot>{{ label }}</slot>
   </button>
 </template>
 
-<style module>
+<style scoped>
 .small_button {
   background-color: #363333;
   padding: 3px 20px;
@@ -33,5 +36,13 @@ defineProps({
 .small_button:hover {
   filter: brightness(160%);
   transition: all 1s ease-in-out;
+}
+
+.generate_password {
+  position: absolute;
+  bottom: 9.02rem;
+  left: 12rem;
+  background-color: #050555;
+  border: 2px solid #0c0b0b;
 }
 </style>
