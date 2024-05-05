b4e68a516241648e095afb26ce8f4ed55e3a192f
aniebietafia <aniebietafia87@gmail.com>
2024-05-05 16:43:58 +0100

    added build for font-awesome

--- brints-estate-frontend/vite.config.js
+++ brints-estate-frontend/vite.config.js
@@ -4,22 +4,15 @@ import vue from "@vitejs/plugin-vue";
 import envCompatible from "vite-plugin-env-compatible";
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-    plugins: [vue()],
-=======
     plugins: [vue(), envCompatible()],
->>>>>>> bb83104d9b2167d7065c7879c930181c513b7dbe
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-<<<<<<< HEAD
-=======
     },
     build: {
         rollupOptions: {
             external: ["@fortawesome/vue-fontawesome"],
         },
->>>>>>> bb83104d9b2167d7065c7879c930181c513b7dbe
     },
 });
