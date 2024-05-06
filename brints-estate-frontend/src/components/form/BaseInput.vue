71ed223bf156a523af2a4d334f325a71472aa9b3
aniebietafia <aniebietafia87@gmail.com>
2024-05-06 07:45:03 +0100

    feat: imported fontawesome icons

--- brints-estate-frontend/src/components/form/BaseInput.vue
+++ brints-estate-frontend/src/components/form/BaseInput.vue
@@ -1,6 +1,19 @@
 <script setup>
 import { ref, watch } from "vue";
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+// Add the icons to the library
+library.add(fas);
+
+defineComponent({
+  components: {
+    FontAwesomeIcon,
+  },
+});
+
 const props = defineProps({
   id: {
     type: String,
