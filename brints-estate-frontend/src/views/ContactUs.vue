055d3e04ced1eb18c37d512b49a1b45106bf7c68
aniebietafia <aniebietafia87@gmail.com>
2024-05-09 12:47:52 +0100

    feat: contact us component

---
+++ brints-estate-frontend/src/views/ContactUs.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeaderBar from "@/components/layout/HeaderBar.vue";
+</script>
+
+<template>
+  <HeaderBar></HeaderBar>
+  <main>
+    <p>Coming Soon</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
