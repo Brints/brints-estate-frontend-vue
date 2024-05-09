219dffda87e9d160ab52346b82f9764a7b763da3
aniebietafia <aniebietafia87@gmail.com>
2024-05-09 12:35:40 +0100

    feat: routes for users

--- brints-estate-frontend/src/router/userRoutes.ts
+++ brints-estate-frontend/src/router/userRoutes.ts
@@ -1,34 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LandingPage from "../views/LandingPage.vue";
+const userRoutes = [
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/users/LoginPage.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () => import("@/views/users/SignupPage.vue"),
+  },
+  {
+    path: "/verify-email",
+    name: "verify-email",
+    component: () => import("@/views/users/EmailVerification.vue"),
+  },
+  {
+    path: "/verify-phone",
+    name: "verify-phone",
+    component: () => import("@/views/users/OTPVerification.vue"),
+  },
+];
 
-const userRouter = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "landing-page",
-      component: LandingPage,
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("../views/users/LoginPage.vue"),
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutUs.vue"),
-    },
-    {
-      path: "/signup",
-      name: "signup",
-      component: () => import("../views/users/SignupPage.vue"),
-    },
-    {
-      path: "/:path(.*)*",
-      component: () => import("../views/NotFound.vue"),
-    },
-  ],
-});
-
-export default userRouter;
+export default userRoutes;
