70fef7612429dafba78263b50f13bc642eb16ae9
aniebietafia <aniebietafia87@gmail.com>
2024-05-09 12:36:12 +0100

    feat: index routes

--- brints-estate-frontend/src/router/index.ts
+++ brints-estate-frontend/src/router/index.ts
@@ -1,44 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LandingPage from "../views/LandingPage.vue";
+
+import baseRoutes from "./baseRoutes";
+import userRoutes from "./userRoutes";
+
+const routes = [...baseRoutes, ...userRoutes];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "landing-page",
-      component: LandingPage,
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("../views/users/LoginPage.vue"),
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutUs.vue"),
-    },
-    {
-      path: "/signup",
-      name: "signup",
-      component: () => import("../views/users/SignupPage.vue"),
-    },
-    {
-      path: "/verify-email",
-      name: "verify-email",
-      component: () => import("../views/users/EmailVerification.vue"),
-    },
-    {
-      path: "/verify-phone",
-      name: "verify-phone",
-      component: () => import("../views/users/OTPVerification.vue"),
-    },
-    {
-      path: "/:path(.*)*",
-      component: () => import("../views/NotFound.vue"),
-    },
-  ],
+  routes,
 });
 
 export default router;
