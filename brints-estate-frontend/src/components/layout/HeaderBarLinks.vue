c08d94132bb4779c59874c7881e9280db469d1df
aniebietafia <aniebietafia87@gmail.com>
2024-05-11 11:29:17 +0100

    feat: added active link class

--- brints-estate-frontend/src/components/layout/HeaderBarLinks.vue
+++ brints-estate-frontend/src/components/layout/HeaderBarLinks.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <header :class="$style.header_links">
+  <header class="header_links">
     <nav className="flex gap-4 items-center">
       <router-link :to="{ name: 'landing-page' }">Home</router-link>
       <router-link :to="{ name: 'about' }">About Us</router-link>
@@ -9,14 +9,14 @@
       <router-link :to="{ name: 'company' }">Company Profile</router-link>
     </nav>
 
-    <nav className="flex gap-4">
+    <nav className="flex gap-4 items-center">
       <router-link :to="{ name: 'signup' }">Register</router-link>
       <router-link :to="{ name: 'login' }">Login</router-link>
     </nav>
   </header>
 </template>
 
-<style module>
+<style scoped>
 .header_links {
   width: 100%;
   display: grid;
@@ -27,15 +27,13 @@
 
 nav a {
   padding: 0.3rem;
+  text-decoration: none;
+  color: inherit;
 }
 
 nav a:hover,
-nav a:active {
+.router-link-active {
   color: #f1a80a;
   border-bottom: 1px solid #f1a80a;
-}
-
-.router-link-active {
-  color: #fff;
 }
 </style>
