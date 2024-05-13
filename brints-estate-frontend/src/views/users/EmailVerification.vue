f703d404d1497aec4b3b19bf61dd3f7340c48afb
aniebietafia <aniebietafia87@gmail.com>
2024-05-13 14:32:54 +0100

    feat: email verification component

--- brints-estate-frontend/src/views/users/EmailVerification.vue
+++ brints-estate-frontend/src/views/users/EmailVerification.vue
@@ -1,47 +1,48 @@
 <script setup>
-import { ref } from "vue";
-import SuccessMessage from "@/components/messages/SuccessMessage.vue";
-import BaseButton from "@/components/buttons/BaseButton.vue";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-let loading = ref(false);
+const router = useRouter();
+
+const loading = ref(true);
+
+// Parse the email and token from the URL query string
+const email = router.currentRoute.value.query.email;
+const token = router.currentRoute.value.query.token;
+
+// Send a GET request function to the backend to verify the email using fetch
+const verifyEmail = async () => {
+  const url = `${import.meta.env.VITE_BACKEND_URL}/user/verify-email?email=${email}&token=${token}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to verify email: ${response.statusText}`);
+    }
+
+    loading.value = false;
+
+    // Redirect to the login page
+    setTimeout(() => {
+      router.push({ name: "login" });
+    }, 2000);
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => {
+  verifyEmail();
+});
 </script>
 
 <template>
-  <div :class="$style.container">
-    <div v-if="loading">
-      <p>Loading...</p>
-      <p>Please wait...</p>
-    </div>
-
-    <div v-else :class="$style.output">
-      <SuccessMessage>
-        <p>Your email has been verified successfully.</p>
-      </SuccessMessage>
-      <!-- Login Button -->
-      <router-link to="/login">
-        <BaseButton type="button" mode="primary">Login</BaseButton>
-      </router-link>
-    </div>
-  </div>
+  <div></div>
 </template>
 
-<style module>
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-.container > * {
-  margin-bottom: 1rem;
-}
-
-.output {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+<style module></style>
