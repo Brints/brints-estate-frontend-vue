ba002bc3f64ad5786ae23b2bad43f9c8eaf73ff8
aniebietafia <aniebietafia87@gmail.com>
2024-05-14 17:09:17 +0100

    feat: success message component

--- brints-estate-frontend/src/components/messages/SuccessMessage.vue
+++ brints-estate-frontend/src/components/messages/SuccessMessage.vue
@@ -1,9 +1,39 @@
-<script setup></script>
+<script setup>
+// import { ref } from "vue";
+
+// const message = ref("");
+
+// const setMessage = (msg) => {
+//   message.value = msg;
+// };
+</script>
 
 <template>
-  <div class="alert alert-success" role="alert">
-    <slot></slot>
+  <div class="alert alert-success">
+    <p><slot></slot></p>
   </div>
 </template>
 
-<style module></style>
+<style scoped>
+.alert {
+  padding: 20px;
+  background-color: #4caf50;
+  color: white;
+  margin-bottom: 15px;
+}
+
+.closebtn {
+  margin-left: 15px;
+  color: white;
+  font-weight: bold;
+  float: right;
+  font-size: 22px;
+  line-height: 20px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.closebtn:hover {
+  color: black;
+}
+</style>
