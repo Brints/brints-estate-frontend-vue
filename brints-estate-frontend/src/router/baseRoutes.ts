55a66a7da891ea47bfbed1c9f7076afe8c204494
aniebietafia <aniebietafia87@gmail.com>
2024-05-16 15:34:19 +0100

    feat: set requires auth to false

--- brints-estate-frontend/src/router/baseRoutes.ts
+++ brints-estate-frontend/src/router/baseRoutes.ts
@@ -5,21 +5,33 @@ const baseRoutes = [
     path: "/",
     name: "landing-page",
     component: LandingPage,
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: "/about",
     name: "about",
     component: () => import("@/views/AboutUs.vue"),
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: "/contact",
     name: "contact",
     component: () => import("@/views/ContactUs.vue"),
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: "/company",
     name: "company",
     component: () => import("@/views/CompanyProfile.vue"),
+    meta: {
+      requiresAuth: false,
+    },
   },
 ];
 
