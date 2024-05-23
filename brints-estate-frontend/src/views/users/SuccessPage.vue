08476733bbbf7ed39604b30fe1ed12f3c677423d
aniebietafia <aniebietafia87@gmail.com>
2024-05-23 12:20:18 +0100

    feat: success page after verifying phone number

---
+++ brints-estate-frontend/src/views/users/SuccessPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SmallButton from "@/components/buttons/SmallButton.vue";
+</script>
+
+<template>
+  <div>
+    <p>Your phone number has been verified successfully.</p>
+    <p>Check your email to verify your email address.</p>
+    <router-link :to="{ name: 'login' }">
+      <SmallButton type="submit"><span className="text-green-400">Get Started</span></SmallButton>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+div {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
