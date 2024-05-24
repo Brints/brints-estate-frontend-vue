e8b6b8c1b46ce224d6d7a560e31173c3a9d01139
aniebietafia <aniebietafia87@gmail.com>
2024-05-24 09:35:55 +0100

    feat: user validations

--- brints-estate-frontend/src/services/validations.ts
+++ brints-estate-frontend/src/services/validations.ts
@@ -1,20 +1,45 @@
-// Email Validation class
-export class EmailValidation {
-  static validEmail(email: string): boolean {
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailPattern.test(email);
-  }
+// export class UserDataValidators {
+//   emailValidator = {
+//     $validator: (value: string) => {
+//       if (!value) return 0;
+//       return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? 1 : 2;
+//     },
+//     $message: ["Email is required", "Must be a valid email address"],
+//   };
 
-  //   static emptyEmail(email: string): boolean {
-  //     return email === "";
-  //   }
-}
+//   passwordValidator = {
+//     $validator: (value: string) => {
+//       if (!value) return 0;
+//       return value.length >= 6 ? 1 : 2;
+//     },
+//     $message: ["Password is required", "Password must be at least 6 characters"],
+//   };
 
-// Field Validation
-export class Validation {
-  static emptyField(field: string): boolean {
-    return field === "";
-  }
-}
+//   fullNameValidator = {
+//     $validator: (value: string) => {
+//       if (!value) return 0;
+//       return value.length >= 3 ? 1 : 2;
+//     },
+//     $message: ["Full name is required", "Full name must be at least 3 characters"],
+//   };
+// }
 
-// Password Validation class
+export const fullNameValidator = {
+  $validator: (value: string) => {
+    // if (!value) return 0;
+    // return value.length >= 3 ? 1 : 2;
+    if (typeof value === "undefined" || value === null || value === "") {
+      return true;
+    }
+    return value.length >= 3 ? false : true;
+  },
+  $message: ["Full name is required", "Full name must be at least 3 characters"],
+};
+
+export const emailValidator = {
+  $validator: (value: string) => {
+    if (!value) return 0;
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? 1 : 2;
+  },
+  $message: ["Email is required", "Must be a valid email address"],
+};
