f34dacc8547e810a27455443e905802dc9fa4f82
aniebietafia <aniebietafia87@gmail.com>
2024-05-30 15:13:12 +0100

    feat: imported listings routes

--- brints-estate-frontend/src/router/index.ts
+++ brints-estate-frontend/src/router/index.ts
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import baseRoutes from "./baseRoutes";
 import userRoutes from "./userRoutes";
+import listingRoutes from "./listingsRoutes";
 
-const routes = [...baseRoutes, ...userRoutes];
+const routes = [...baseRoutes, ...userRoutes, ...listingRoutes];
 
 const router = createRouter({
   linkActiveClass: "active-link",
