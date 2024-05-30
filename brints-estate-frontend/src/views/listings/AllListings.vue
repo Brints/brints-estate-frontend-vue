6ef3b8e4b95c7cd9636d5d120c7ae1c9fa24467a
aniebietafia <aniebietafia87@gmail.com>
2024-05-30 15:09:37 +0100

    feat: initializing all listings component

---
+++ brints-estate-frontend/src/views/listings/AllListings.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>All Listings</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
