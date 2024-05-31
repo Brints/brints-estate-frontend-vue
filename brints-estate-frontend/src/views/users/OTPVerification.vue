1979dddfc579e63681dbdeb8de7acc6103faf548
aniebietafia <aniebietafia87@gmail.com>
2024-05-31 08:45:04 +0100

    feat: validating input using vuelidate

--- brints-estate-frontend/src/views/users/OTPVerification.vue
+++ brints-estate-frontend/src/views/users/OTPVerification.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength, maxLength, helpers, numeric } from "@vuelidate/validators";
 
 import BaseForm from "@/components/form/BaseForm.vue";
 import BaseInput from "@/components/form/BaseInput.vue";
 import BaseButton from "@/components/buttons/BaseButton.vue";
+import ValidationError from "@/components/messages/ValidationError.vue";
+import ErrorMessage from "@/components/messages/ErrorMessage.vue";
 
 import { useUserStore } from "@/stores/userStore";
 import { useRouter } from "vue-router";
@@ -16,6 +20,21 @@ const formData = ref({
   otp: "",
   phone: "",
 });
+const errorMessage = ref("");
+
+const rules = {
+  otp: {
+    required: helpers.withMessage("OTP is required.", required),
+    minLength: helpers.withMessage("OTP should not be less than 6 numbers", minLength(6)),
+    maxLength: helpers.withMessage("OTP should not be more than 6 numbers", maxLength(6)),
+    numeric: helpers.withMessage("OTP should be numbers", numeric),
+  },
+  phone: {
+    required: helpers.withMessage("Phone number is required.", required),
+  },
+};
+
+const v$ = useVuelidate(rules, formData);
 
 const loading = ref(false);
 
@@ -24,7 +43,8 @@ formData.value.phone = userStore.phoneNumber;
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-const handleSubmit = async () => {
+const verifyOTP = async () => {
+  if (!(await v$.value.$validate())) return;
   loading.value = true;
 
   try {
@@ -36,23 +56,57 @@ const handleSubmit = async () => {
     }
   } catch (error) {
     console.error(error);
+    const response = error.response.data;
+    errorMessage.value = response.error.message;
   } finally {
     loading.value = false;
   }
 };
 
-const handleInput = (e) => {
-  formData.value[e.target.name] = e.target.value;
-};
+// const handleSubmit = async () => {
+// loading.value = true;
+
+// try {
+//   const response = await axios.post(`${backendUrl}/user/verify-phone`, formData.value);
+//   const { data } = response;
+
+//   if (data.statusCode === 200) {
+//     router.push({ name: "success" });
+//   }
+// } catch (error) {
+//   console.error(error);
+// } finally {
+//   loading.value = false;
+// }
+// };
+
+// const handleInput = (e) => {
+//   formData.value[e.target.name] = e.target.value;
+// };
 </script>
 
 <template>
   <div :class="$style.wrapper">
-    <BaseForm @submit.prevent="handleSubmit" legend="OTP Verification">
-      <template #content>
-        <BaseInput id="otp" label="OTP" type="text" name="otp" placeholder="Enter OTP" @input="handleInput" />
-        <BaseButton type="submit">Verify OTP</BaseButton>
-      </template>
+    <div v-if="errorMessage" :class="$style.error_message">
+      <ErrorMessage :message="errorMessage" />
+    </div>
+    <BaseForm @submit="verifyOTP">
+      <fieldset>
+        <legend>OTP Verficication</legend>
+        <div :class="$style.otp_input">
+          <BaseInput
+            v-model="v$.otp.$model"
+            id="otp"
+            label="OTP"
+            type="text"
+            name="otp"
+            placeholder="Enter OTP"
+            :isInvalid="v$.$invalid"
+          />
+          <ValidationError :model="v$.otp"></ValidationError>
+        </div>
+        <BaseButton type="submit" :disabled="v$.$invalid">Verify OTP</BaseButton>
+      </fieldset>
     </BaseForm>
   </div>
 </template>
@@ -65,5 +119,26 @@ const handleInput = (e) => {
   height: 100vh;
   max-width: 500px;
   margin: 0 auto;
+}
+
+fieldset {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px 20px;
+}
+
+legend {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+  padding: 0 10px;
+}
+
+.otp_input {
+  margin-bottom: 1rem;
+}
+
+.error_message {
+  color: #ca0b0b;
 }
 </style>
