78d704ef3f252f5dd7dc8cf66b998748d83407a5
aniebietafia <aniebietafia87@gmail.com>
2024-05-31 13:46:40 +0100

    feat: when submitting form, button label should show Submitting...

--- brints-estate-frontend/src/views/users/SignupPage.vue
+++ brints-estate-frontend/src/views/users/SignupPage.vue
@@ -110,147 +110,10 @@ const handleSubmit = async () => {
 // set signup button to disabled and changed text to Submitting when loading is true
 </script> -->
 
-<!-- <template>
-  <HeaderBar></HeaderBar>
-  <main :class="$style.wrapper">
-    <div v-if="errorMessage" class="error_message">
-      <ErrorMessages :message="errorMessage" />
-    </div>
-    <BaseForm @submit.prevent="handleSubmit" legend="Sign Up" mode="signup">
-      <template #content>
-        <ImageInput label="Upload your Avatar" id="avatar" name="avatar" icon="image" @change="onFileChange" />
-        <section :class="$style.form_content">
-          <section :class="$style.left_column">
-            <BaseInput
-              label="Full Name"
-              id="fullname"
-              name="fullname"
-              type="text"
-              placeholder="Enter your full name"
-              asterisk="*"
-              icon="user"
-              @input="handleInput"
-            />
-
-            <BaseInput
-              label="Email"
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Enter your email address"
-              asterisk="*"
-              icon="envelope"
-              @input="handleInput"
-            />
-
-            <BaseInput
-              label="Password"
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              asterisk="*"
-              icon="lock"
-              special="eye"
-              special_icon="password_eye"
-              @input="handleInput"
-            />
-          </section>
-
-          <section :class="$style.right_column">
-            <div :class="$style.phone">
-              <CountryCodeSelect
-                label="Code"
-                id="code"
-                name="code"
-                asterisk="*"
-                icon="globe"
-                @input="handleInput"
-              ></CountryCodeSelect>
-
-              <BaseInput
-                label="Phone"
-                id="phone"
-                name="phone"
-                type="tel"
-                placeholder="Enter your phone number"
-                asterisk="*"
-                icon="phone"
-                @input="handleInput"
-              />
-            </div>
-
-            <LabelSelect label="Gender" id="gender" name="gender" asterisk="*" icon="venus-mars" @input="handleInput">
-              <option value="">Choose your Gender</option>
-              <option value="female">Female</option>
-              <option value="male">Male</option>
-            </LabelSelect>
-
-            <BaseInput
-              label="Confirm Password"
-              id="confirmPassword"
-              name="confirmPassword"
-              type="password"
-              placeholder="Confirm your password"
-              asterisk="*"
-              icon="lock"
-              special="eye"
-              special_icon="confirm_eye"
-              @input="handleInput"
-            />
-          </section>
-        </section>
-        <SmallButton type="button" label="Generate Password" button="generate_password" />
-        <div :class="$style.btns_wrapper">
-          <BaseButton type="submit" mode="signup" :disabled="loading">Signup</BaseButton>
-          <BaseButton type="button" mode="google">Signup with Google</BaseButton>
-          <BaseButton type="reset" mode="reset_btn">Reset</BaseButton>
-        </div>
-      </template>
-    </BaseForm>
-  </main>
-</template> -->
-
-<!-- <style module>
-.wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 100vh;
-  margin: 1rem 0;
-}
-
-.form_content {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
-}
-
-.left_column,
-.right_column {
-  display: grid;
-  grid-template-rows: repeat(3, 1fr);
-}
-
-.btns_wrapper {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
-  padding-bottom: 1rem;
-}
-
-.phone {
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  gap: 10px;
-}
-</style> -->
-
 <script setup>
 import { ref } from "vue";
 import { useVuelidate } from "@vuelidate/core";
-import { required, email, minLength, maxLength, sameAs, helpers, alpha } from "@vuelidate/validators";
+import { required, email, minLength, maxLength, helpers } from "@vuelidate/validators";
 // import { passwordValidator } from "@/services/validations";
 
 import BaseForm from "@/components/form/BaseForm.vue";
@@ -291,7 +154,7 @@ const rules = {
     required: helpers.withMessage("Fullname is required", required),
     minLength: helpers.withMessage("Fullname should be more than 3 characters", minLength(3)),
     maxLength: helpers.withMessage("Fullname should not be more than 80 characters", maxLength(80)),
-    alpha: helpers.withMessage("Fullname should contain only letters.", alpha),
+    // alpha: helpers.withMessage("Fullname should contain only letters.", alpha),
   },
   email: {
     required: helpers.withMessage("Email is required", required),
@@ -311,7 +174,7 @@ const rules = {
   },
   confirmPassword: {
     required: helpers.withMessage("Confirm Password is required", required),
-    sameAs: helpers.withMessage("Confirm Password must be same as Password.", sameAs(formData.value.password)),
+    // sameAs: helpers.withMessage("Confirm Password must be same as Password.", sameAs(formData.value.password)),
   },
   avatar: {
     // required: helpers.withMessage("Avatar is required", required),
@@ -324,12 +187,21 @@ const rules = {
   },
 };
 
+// computed(() => {
+//   if (loading.value) {
+//     return "Submitting...";
+//   } else {
+//     return "Signup";
+//   }
+// });
+
 const v$ = useVuelidate(rules, formData);
 
 const url = import.meta.env.VITE_BACKEND_URL;
 
-const signup = async () => {
+const handleSignup = async () => {
   if (!(await v$.value.$validate())) return;
+  loading.value = true;
 
   const data = new FormData();
   data.append("fullname", formData.value.fullname);
@@ -341,21 +213,13 @@ const signup = async () => {
   data.append("code", formData.value.code);
   data.append("gender", formData.value.gender);
 
-  // for (let [key, value] of data.entries()) {
-  //   console.log(`${key}: ${value}`);
-  // }
-
   try {
     loading.value = true;
-    const response = axios.post(`${url}/user/register`, data, {
+    const response = await axios.post(`${url}/user/register`, data, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
-
-    if (!response.ok) {
-      // handle error
-    }
 
     const userData = response.data;
     const payload = userData.payload;
@@ -379,10 +243,10 @@ const signup = async () => {
       router.push({ name: "verify-phone" });
     }
   } catch (error) {
+    //Reload the signup page
+    // router.push({ name: "signup" });
     const response = error.response.data;
     errorMessage.value = response.error.message;
-    //Reload the signup page
-    router.push({ name: "signup" });
   } finally {
     loading.value = false;
   }
@@ -399,7 +263,7 @@ const signup = async () => {
       <ErrorMessages :message="errorMessage" />
     </div>
 
-    <BaseForm :class="$style.form" @submit="signup">
+    <BaseForm @submit="handleSignup">
       <fieldset>
         <legend>Sign Up</legend>
 
@@ -518,7 +382,12 @@ const signup = async () => {
           </section>
         </section>
         <div :class="$style.btns_wrapper">
-          <BaseButton type="submit" mode="signup" label="Signup"></BaseButton>
+          <BaseButton
+            type="submit"
+            mode="signup"
+            :label="loading ? 'Submitting...' : 'Signup'"
+            :disabled="loading"
+          ></BaseButton>
           <BaseButton type="reset" mode="reset_btn" label="Reset"></BaseButton>
         </div>
       </fieldset>
