ec7a31a8b022b2804948d1d0ef3e94b284067be6
aniebietafia <aniebietafia87@gmail.com>
2024-06-01 18:00:15 +0100

    feat: added signup function in the pinia store

--- brints-estate-frontend/src/stores/userStore.ts
+++ brints-estate-frontend/src/stores/userStore.ts
@@ -1,10 +1,61 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import axios from "axios";
 
 export const useUserStore = defineStore("user", () => {
   const phoneNumber = ref("");
+  const loading = ref(false);
+  const error = ref(null);
+
+  const url = import.meta.env.VITE_BACKEND_URL;
+
+  const signup = async (data: any) => {
+    const formData = new FormData();
+    formData.append("fullname", data.value.fullname);
+    formData.append("email", data.value.email);
+    formData.append("password", data.value.password);
+    formData.append("phone", data.value.phone);
+    formData.append("gender", data.value.gender);
+    formData.append("avatar", data.value.avatar);
+    formData.append("confirmPassword", data.value.confirmPassword);
+    formData.append("code", data.value.code);
+    loading.value = true;
+    try {
+      loading.value = true;
+
+      // Call your API here
+      // const response = await fetch(`${url}/user/register`, {
+      //   method: "POST",
+      //   body: formData,
+      //   headers: {
+      //     Accept: "multipart/form-data"
+      //   }
+      // })
+
+      // const responseData = await response.json();
+
+      const response = await axios.post(`${url}/user/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const userData = response.data;
+      const payload = userData.payload;
+      phoneNumber.value = payload.phone;
+    } catch (error) {
+      console.error(error);
+      const response = error.response.data;
+      error.value = response.error.message;
+    } finally {
+      loading.value = false;
+    }
+  };
 
   return {
     phoneNumber,
+    loading,
+    error,
+    signup,
   };
 });
