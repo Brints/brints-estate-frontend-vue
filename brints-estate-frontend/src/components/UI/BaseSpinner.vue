42909ace54fb263ec76038d9746bedbf2ba14cc0
aniebietafia <aniebietafia87@gmail.com>
2024-06-03 11:10:03 +0100

    feat: setting up the spinner template

---
+++ brints-estate-frontend/src/components/UI/BaseSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="$style.spinner">
+    <div :class="$style.lds_roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style module></style>
