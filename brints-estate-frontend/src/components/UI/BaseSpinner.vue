82a74e184ee6da8b0db31076cf3b57522cc7e962
aniebietafia <aniebietafia87@gmail.com>
2024-06-03 11:19:20 +0100

    feat: adding css for lds roller contents

--- brints-estate-frontend/src/components/UI/BaseSpinner.vue
+++ brints-estate-frontend/src/components/UI/BaseSpinner.vue
@@ -32,4 +32,15 @@
   animation: lds_roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   transform-origin: 40px 40px;
 }
+
+.lds_roller div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #3d008d;
+  margin: -4px 0 0 -4px;
+}
 </style>
