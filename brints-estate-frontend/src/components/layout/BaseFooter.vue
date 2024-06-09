8aea1a176c3dd3a226b88b605c03caf0d3825c98
aniebietafia <aniebietafia87@gmail.com>
2024-06-09 15:39:10 +0100

    feat: adding date feature component to footer

---
+++ brints-estate-frontend/src/components/layout/BaseFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import BaseFooterDate from "./BaseFooterDate.vue";
+</script>
+
+<template>
+  <footer class="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
+    <div class="container px-6 py-4 mx-auto">
+      <BaseFooterDate />
+    </div>
+  </footer>
+</template>
+
+<style module></style>
