5db87d5e3ec077d908607a1c496233da6eb97275
aniebietafia <aniebietafia87@gmail.com>
2024-06-09 15:37:46 +0100

    feat: formatting date in the footer

---
+++ brints-estate-frontend/src/components/layout/BaseFooterDate.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+const date = ref(new Date().getFullYear());
+const year = ref(date.value);
+const month = ref(date.value);
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+month.value = months[new Date().getMonth()];
+year.value = new Date().getFullYear();
+
+setInterval(() => {
+  date.value = new Date().getFullYear();
+  month.value = months[new Date().getMonth()];
+  year.value = date.value;
+}, 1000);
+</script>
+
+<template>
+  <div class="text-center text-sm text-gray-500 dark:text-gray-400">
+    <span>&copy; {{ month }} {{ year }} Brints Estate</span>
+    <!-- <span>&copy; 2021 Brints Estate</span> -->
+  </div>
+</template>
+
+<style module></style>
