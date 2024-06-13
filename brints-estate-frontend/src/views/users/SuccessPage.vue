bb470a12b348c13356ed8924e44e136960f85211
aniebietafia <aniebietafia87@gmail.com>
2024-06-13 10:43:45 +0100

    feat: add button component

--- brints-estate-frontend/src/views/users/SuccessPage.vue
+++ brints-estate-frontend/src/views/users/SuccessPage.vue
@@ -7,7 +7,7 @@ import SmallButton from "@/components/buttons/SmallButton.vue";
     <p>Your phone number has been verified successfully.</p>
     <p>Check your email to verify your email address.</p>
     <router-link :to="{ name: 'login' }">
-      <SmallButton type="submit"><span className="text-green-400">Continue</span></SmallButton>
+      <SmallButton type="submit" label="Continue"><span className="text-green-400"></span></SmallButton>
     </router-link>
   </div>
 </template>
