1a89e33a7591bed25fb31081f4b8f95ebb416a6b
aniebietafia <aniebietafia87@gmail.com>
2024-06-16 15:28:40 +0100

    feat: creating listings header

---
+++ brints-estate-frontend/src/components/layout/ListingsHeader.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { computed } from "vue";
+import BaseLogo from "@/components/layout/BaseLogo.vue";
+
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const handleLogout = () => {
+  authStore.logout();
+
+  router.push({ name: "login" });
+};
+
+const displayName = computed(() => {
+  return authStore.user.fullname;
+});
+// const isLoggedIn = computed(() => {
+//   return authStore.isLoggedIn;
+// });
+const displayProfilePic = computed(() => {
+  if (authStore.user.avatar.length > 0) {
+    return authStore.user.avatar[0].url;
+  } else {
+    return "";
+  }
+});
+const displayInitials = computed(() => {
+  const fullname = authStore.user.fullname.split(" ");
+  return `${fullname[0].charAt(0).toUpperCase()}${fullname[1].charAt(0).toUpperCase()}`;
+});
+const displayAvatarAlt = computed(() => {
+  if (authStore.user.avatar.length > 0) {
+    return authStore.user.avatar[0];
+  } else {
+    return authStore.user.fullname;
+  }
+});
+
+authStore.loadTokenFromLocalStorage();
+</script>
+
+<template>
+  <header :class="[$style.main_heading, 'flex p-5 items-center']">
+    <div :class="['container', 'items-center']">
+      <BaseLogo></BaseLogo>
+    </div>
+
+    <div class="container">
+      <nav class="flex justify-between items-center py-4">
+        <div>
+          <router-link to="/">Home</router-link>
+          <router-link to="/listings">Listings</router-link>
+        </div>
+
+        <div class="flex gap-4 items-center overflow-hidden">
+          <div :class="[$style.profile_picture, 'bg-sky-900 flex gap-4 items-center justify-center overflow-hidden']">
+            <img
+              v-if="displayProfilePic"
+              :src="displayProfilePic"
+              :alt="displayAvatarAlt"
+              class="rounded-full object-cover h-full w-full"
+            />
+
+            <div v-else class="rounded-full flex justify-center items-center">
+              {{ displayInitials }}
+            </div>
+          </div>
+
+          <div>
+            <p>{{ displayName }}</p>
+          </div>
+          <!-- <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
+          <button @click="handleLogout" v-if="isLoggedIn">Logout</button> -->
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style module>
+.main_heading {
+  background-color: #333;
+  color: #fff;
+}
+
+.profile_picture {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+.profile_picture img {
+  width: 100%;
+  height: 100%;
+}
+</style>
