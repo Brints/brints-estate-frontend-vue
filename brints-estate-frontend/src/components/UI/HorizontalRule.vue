7b63e030852bddae3df56a2b0b048e80f7331b53
aniebietafia <aniebietafia87@gmail.com>
2024-06-18 13:12:04 +0100

    feat: adding horizontal rule component

---
+++ brints-estate-frontend/src/components/UI/HorizontalRule.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <hr class="border-t-2 border-gray-200 my-8" />
+</template>
+
+<style scoped>
+hr {
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(to right, transparent, #ccc, transparent);
+}
+</style>
