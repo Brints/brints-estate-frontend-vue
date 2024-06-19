855be1996f50a963326f6a3f680db375277b207f
aniebietafia <aniebietafia87@gmail.com>
2024-06-19 10:31:54 +0100

    feat: component to display user's name

---
+++ brints-estate-frontend/src/components/UI/DisplayUserName.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted, computed } from "vue";
+
+import { useAuthStore } from "@/stores/authStore";
+
+const authStore = useAuthStore();
+
+const displayName = computed(() => {
+  return authStore.user.fullname.split(" ")[0];
+});
+
+onMounted(() => {
+  authStore.loadTokenFromLocalStorage();
+});
+</script>
+
+<template>
+  <section>
+    {{ displayName }}
+  </section>
+</template>
+
+<style scoped>
+section {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #c9ab00;
+}
+</style>
