1f933112c9533101d84c801995c5ee9eb34789d9
aniebietafia <aniebietafia87@gmail.com>
2024-06-19 10:31:12 +0100

    feat: displaying greeting and username

--- brints-estate-frontend/src/views/listings/AllListings.vue
+++ brints-estate-frontend/src/views/listings/AllListings.vue
@@ -1,28 +1,57 @@
 <script setup>
-import { useAuthStore } from "@/stores/authStore";
-const authStore = useAuthStore();
-
-authStore.loadTokenFromLocalStorage();
+import ListingsHeader from "@/components/layout/ListingsHeader.vue";
+import DisplayUserName from "@/components/UI/DisplayUserName.vue";
+import GreetingUser from "@/components/UI/GreetingUser.vue";
+import CurrentTime from "@/components/UI/CurrentTime.vue";
 </script>
 
 <template>
-  <div>
-    <section v-if="authStore.isLoggedIn">
-      {{ authStore.user.fullname }}
+  <div :class="$style.container">
+    <div class="header">
+      <ListingsHeader></ListingsHeader>
+    </div>
+
+    <section :class="$style.main">
+      <div className="flex justify-between items-center mb-4">
+        <div :class="$style.greeting">
+          <span><GreetingUser></GreetingUser></span>
+          <span className="items-center"><DisplayUserName></DisplayUserName></span>
+        </div>
+        <div className="flex gap-1 items-center">
+          <span>Your Time: </span>
+          <span><CurrentTime></CurrentTime></span>
+        </div>
+      </div>
+
+      <div :class="$style.trending">
+        <h1>Trending Listings</h1>
+      </div>
+
+      <div :class="$style.recommended">
+        <h1>Recommended Listings</h1>
+      </div>
+
+      <div :class="$style.listings">
+        <h1>All Listings</h1>
+      </div>
     </section>
   </div>
 </template>
 
-<style scoped>
-div {
-  width: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style module>
+.container {
+  width: 100%;
   height: 100vh;
 }
 
-section {
-  width: 80%;
+.main {
+  padding: 0 1rem;
+}
+
+.greeting {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.5rem;
 }
 </style>
