99db47145797a4c037db567bc979a327d339447a
aniebietafia <aniebietafia87@gmail.com>
2024-06-19 10:32:36 +0100

    feat: component to display greeting based on time of day

---
+++ brints-estate-frontend/src/components/UI/GreetingUser.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { computed } from "vue";
+
+// const emojis = ["👋", "🌞", "🌜", "🌅", "🌇", "🌄", "🌆", "🌃", "🌌", "🌠", "🌈"];
+
+const greeting = computed(() => {
+  const date = new Date();
+  const hours = date.getHours();
+  let greeting = "";
+
+  if (hours < 12) {
+    greeting = "Good Morning";
+  } else if (hours < 18) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+
+  return greeting;
+});
+</script>
+
+<template>
+  <div>
+    {{ greeting }}
+  </div>
+</template>
+
+<style scoped>
+div {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #333;
+}
+
+div::before {
+  content: "👋 ";
+}
+
+div::after {
+  content: "!";
+}
+
+div::before,
+div::after {
+  font-size: 2rem;
+}
+
+@media (max-width: 768px) {
+  div {
+    font-size: 1.2rem;
+  }
+
+  div::before,
+  div::after {
+    font-size: 1.5rem;
+  }
+}
+</style>
