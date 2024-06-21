5c7146d50e87db1adbccfd0094e869efaf8fc1e3
aniebietafia <aniebietafia87@gmail.com>
2024-06-21 10:22:52 +0100

    feat: initializing sign up card

---
+++ brints-estate-frontend/src/views/base/SignUpSignInCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BaseCard from "../../components/UI/BaseCard.vue";
+import BaseDialog from "../../components/UI/BaseDialog.vue";
+import GoogleSignup from "@/components/UI/GoogleSignup.vue";
+</script>
+
+<template>
+  <div>
+    <BaseCard>
+      <BaseDialog title="" :show="true" fixed><GoogleSignup></GoogleSignup></BaseDialog>
+    </BaseCard>
+  </div>
+</template>
+
+<style scoped></style>
