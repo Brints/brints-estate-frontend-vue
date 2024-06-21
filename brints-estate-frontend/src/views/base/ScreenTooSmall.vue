e88f1c8f9df70c01db826b92e53f4d694dd20bf5
aniebietafia <aniebietafia87@gmail.com>
2024-06-21 10:22:03 +0100

    feat: initializing screen too small

---
+++ brints-estate-frontend/src/views/base/ScreenTooSmall.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="screen-too-small">
+    <h1>Screen Too Small</h1>
+    <p>Sorry, your screen is too small to view this page. Please use a larger screen.</p>
+  </div>
+</template>
+
+<style scoped>
+.screen-too-small {
+  display: none;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  font-size: 1.5rem;
+  background-color: #f8f9fa;
+  color: #495057;
+}
+</style>
