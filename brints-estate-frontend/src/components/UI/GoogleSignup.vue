331f8a1416665e1cfdbb51484694afbb9013b381
aniebietafia <aniebietafia87@gmail.com>
2024-06-21 10:14:04 +0100

    feat: initializing google signup component

---
+++ brints-estate-frontend/src/components/UI/GoogleSignup.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="handleGoogleSignup">
+      Sign up with Google
+    </button>
+  </div>
+</template>
