560882d796f8f56b3bbfdfd3a1991dd177e78c54
aniebietafia <aniebietafia87@gmail.com>
2024-06-21 10:31:45 +0100

    cleaning up comments

--- brints-estate-frontend/src/main.ts
+++ brints-estate-frontend/src/main.ts
@@ -6,23 +6,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-// import the fontawesome core
-// import { library } from "@fortawesome/fontawesome-svg-core";
-
-// import fontawesome icon components
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-// import the free solid icons
-// import { fas } from "@fortawesome/free-solid-svg-icons";
-
-// add the imported icons to the library
-// library.add(fas);
-
 const app = createApp(App);
 
-// make the fontawesome icon component available globally
 app.use(createPinia());
 app.use(router);
-// app.component("FontAwesomeIcon", FontAwesomeIcon);
 
 app.mount("#app");
